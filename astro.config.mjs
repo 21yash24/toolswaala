d31982b1bb53959647cd1f691216c8bd61d3f677
yash jha <yashjha@yashs-MacBook-Air.local>
2026-08-18 22:08:44 +0530

    Perf: Inline all stylesheets into HTML

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.toolswaala.in',
+  build: {
+    inlineStylesheets: 'always'
+  },
   integrations: [react(), sitemap()],
 
   vite: {
